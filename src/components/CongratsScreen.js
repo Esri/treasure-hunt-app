97fc5071d730422f8c33cad4c7a8e6c1ea65c818
leebock <lbock@esri.com>
2022-05-20 09:48:31 -0400

    some CongratsScreen fixes: 1) had some invalid class names, so I implemented as custom styles.  2) changed element id from "intro" to "congrats" 3) tweaked shrink/grow behavior for left/right panels

--- src/components/CongratsScreen.js
+++ src/components/CongratsScreen.js
@@ -15,21 +15,21 @@ export const CongratsScreen = ({
 
     className = className+" d-flex justify-content-center align-items-center"
     return(
-        <div id="intro" className={className} style={style}>
+        <div id="congrats" className={className} style={style}>
             <div id="inner" 
-                className="h-auto mh-95 w-95 position-relative overflow-hidden border bg-white ms-2 me-2 p-2"
-                style={{maxWidth: "1000px"}}>
+                className="h-auto position-relative overflow-hidden border bg-white ms-2 me-2 p-2"
+                style={{width: "95%", maxWidth: "1000px", maxHeight: "95%"}}>
                 <div className="d-flex justify-content-end">
                     <button className="btn btn-close" 
                             onClick={()=>{dismiss()}}></button>
                 </div>
                 <div className="d-flex">
-                    <div className="flex-grow-1 d-flex flex-column position-relative overflow-hidden p-2 m-0 m-sm-2 me-sm-1"
+                    <div className="flex-grow-1 flex-shrink-0 d-flex flex-column position-relative overflow-hidden p-2 m-0 m-sm-2 me-sm-1"
                         style={{
                             flexBasis: "50%",
                             backgroundColor: "rgba(255,255,255,0.8)"
                             }}>
-                            <div className="flex-grow-1 flex-shrink-1 overflow-y-auto">
+                            <div className="flex-grow-1 overflow-y-auto">
                                 <h1 className="display-5 fw-bold">Congratulations!</h1>
                                 <p className="h5 mb-5" style={{fontWeight: 400}}>You have successfully completed the <strong>{title}</strong> Treasure Hunt!</p>
                                 <p className="h5" style={{fontWeight: 400}}>In recognition of your excellence, you can:</p>
@@ -60,7 +60,7 @@ export const CongratsScreen = ({
                                 </ul>
                             </div>
                     </div>
-                    <div className="flex-grow-1 d-none d-sm-block m-0 m-sm-2 ms-sm-1" 
+                    <div className="d-none d-sm-block m-0 m-sm-2 ms-sm-1" 
                         style={{
                             flexBasis: "50%",
                             backgroundImage: `url(${hero})`,
